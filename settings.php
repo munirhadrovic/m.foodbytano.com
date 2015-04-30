e28e2cbc65728c032537c12f7c5322f19e0e313e
anes89cok <anes89cok@gmail.com>
2015-04-30 15:22:11 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="204 W Loveland Ave, Loveland, Ohio 45140"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Ohio";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=5136838266;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
